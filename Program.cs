4982c23445dfdadfd4d6cc09f8675be59bf18d29
Pritesh Zaver <priteshzaver@gmail.com>
2023-03-03 10:36:37 -0600

    phase8

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace PlanYourHeist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist");
+            Console.WriteLine("");
+            Console.Write("Please enter the bank difficulty:");
+            int bankDifficulty = int.Parse(Console.ReadLine());
 
             string name = "Pritesh";
             Dictionary<string, TeamMember> team = new Dictionary<string, TeamMember>();
@@ -35,12 +38,11 @@ namespace PlanYourHeist
             }
             Console.Write("How many trial runs are needed?");
             int trials = int.Parse(Console.ReadLine());
+            int successes = 0;
+            int fails = 0;
 
             for (int i = 0; i < trials; i++)
             {
-
-                int bankDifficulty = 100;
-
                 Random random = new Random();
                 int teamLuck = random.Next(-10, 11);
                 bankDifficulty += teamLuck;
@@ -51,12 +53,18 @@ namespace PlanYourHeist
                 if (totalSkill >= bankDifficulty)
                 {
                     Console.WriteLine("You have successfully broken in and stolen some shit!");
+                    bankDifficulty -= teamLuck;
+                    successes++;
                 }
                 else
                 {
                     Console.WriteLine("You couldn't break in so you go to jail bad boy!");
+                    bankDifficulty -= teamLuck;
+                    fails++;
                 }
             }
+            Console.WriteLine("");
+            Console.WriteLine($"The program succeeded {successes} times and failed {fails} times.");
         }
     }
 }
